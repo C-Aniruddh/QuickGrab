35aa553a22090b511139ec04edf72b382bbef305
C-Aniruddh <aniruddh.chandratre@gmail.com>
2020-05-08 18:33:47 +0530

    impetus: initial app

---
+++ android/app/src/main/kotlin/com/impetus/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.impetus.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:app/screens/boilerplate.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Impetus India',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
